b68f28a5307de9a2643ba2c4266074896a3d1e64
Jean-Francois To <jeanfrancoisto@hotmail.com>
2020-05-28 20:20:14 -0400

    implemented click and drag

---
+++ src/components/ScheduleSlot.vue
@@ -0,0 +1,53 @@
+<template>
+  <div @mouseover="fillSlot" :class="{ active: filled }">
+		.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScheduleSlot',
+
+	props: {
+		dragToggle: Boolean,
+	},
+
+	data() {
+		return {
+			filled: false,
+		}
+
+
+	},
+
+
+	methods: {
+		fillSlot: function(){
+			if (this.dragToggle) {
+				this.filled = !this.filled;
+			}
+		}
+	}
+
+	
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.active {
+	background-color: green;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/components/TheSchedule.vue
+++ src/components/TheSchedule.vue
@@ -1,6 +1,6 @@
 <template>
   <div class=schedule>
-		<table>
+		<table @mousedown="dragToggleOn" @mouseup="dragToggleOff">
 			<thead>
 				<tr>
 					<th></th>
@@ -13,7 +13,11 @@
 				<tr v-for="time_index in 40" :key="time_index">
 					<td v-if="(time_index-1) % 4 === 0" rowspan="4">
 						{{ 8 +((time_index-1) / 4)}}</td>
-					<td v-for="(day, day_index) in week" :key="day_index">. </td>
+					<td 
+						v-for="(day, day_index) in week"
+						:key="day_index">
+						<ScheduleSlot :dragToggle=dragToggle></ScheduleSlot>	
+					</td>
 				</tr>
 
 			</tbody>
@@ -23,16 +27,31 @@
 </template>
 
 <script>
+import ScheduleSlot from './ScheduleSlot.vue'
 export default {
   name: 'TheSchedule',
+	components: {
+		ScheduleSlot,
+	},
 
 	data() {
 		return {
 			week: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
 			times: [8,9,11,12,13,14,15,16,17],
-
-
+			dragToggle: false,
 		}
+
+	},
+
+
+	methods: {
+		dragToggleOn: function() {
+			this.dragToggle = true;
+		},
+
+		dragToggleOff: function() {
+			this.dragToggle = false;
+		},
 
 
 	}
@@ -47,9 +66,18 @@ export default {
 	margin: auto;
 	width: 50%;
 }
+
 table, th, td {
 	border: 1px solid white;
+}
 
+table {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+		user-select: none;
 }
 
 
